1971f869706c65ab4fabf117e2dfa1117e10df0e
cheficha <stefanzhekov95@gmail.com>
2017-11-23 20:35:48 +0200

    MaxSumOf3x#MatrixWORKING

--- MaxSumMultiArray/MaxSumMultiArray/Program.cs
+++ MaxSumMultiArray/MaxSumMultiArray/Program.cs
@@ -23,20 +23,22 @@ namespace MaxSumMultiArray
                 }
             }
             int bestSum = int.MinValue;
-            int sum = 0;
+           
             for (int row = 0; row < matrix.GetLength(0)-2; row++)
             {
                 for (int col = 0; col < matrix.GetLength(1)-2 ; col++)
                 {
-                    sum = matrix[row , col] + matrix[row , col + 1] + matrix[row, col+2]+
+                    int sum = matrix[row , col] + matrix[row , col + 1] + matrix[row, col+2]+
                          matrix[row + 1, col] + matrix[row+1 , col+1] + matrix[row + 1, col+2]+
-                          matrix[row + 2, col] + matrix[row + 2, col+1]+ matrix[row + 2, col + 2]; 
+                          matrix[row + 2, col] + matrix[row + 2, col+1]+ matrix[row + 2, col + 2];
+                    if (sum > bestSum)
+                    {
+                        bestSum = sum;
+                    }
                 }
+                
             }
-            if (sum>bestSum)
-            {
-                bestSum = sum;
-            }
+         
             Console.WriteLine(bestSum);
 
         }
