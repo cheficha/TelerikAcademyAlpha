c03072a305f03137f362107bff4724a270a620a5
cheficha <stefanzhekov95@gmail.com>
2017-11-23 10:03:24 +0200

    no message

---
+++ ThirdExer/ThirdExer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirdExer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] field = Console.ReadLine().Split(new[] { ", " },StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int numberOfdirections = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfdirections; i++)
+            {
+                int position = 0;
+                int carrots = 0;               
+                int[] directions = Console.ReadLine().Split(new[] { ", " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+               
+
+            }
+
+        }
+    }
+}
