333cd0044cbe508c36981eea07381cb8bea10e1b
cheficha <stefanzhekov95@gmail.com>
2017-11-26 17:52:41 +0200

    StringLenghts-20

---
+++ StringLength/StringLength/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringLength
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            StringBuilder str = new StringBuilder(text);
+            if (str.Length >= 20)
+            {
+                Console.WriteLine(text);
+            }
+            else if(str.Length<20)
+            {
+                while (str.Length!=20)
+                {
+                    str.Append("*");
+                }
+                Console.WriteLine(str.ToString());
+            }
+           
+        }
+    }
+}
