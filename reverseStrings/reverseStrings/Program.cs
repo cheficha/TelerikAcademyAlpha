30f680c0a77334e1e09037bd1ef3e4cdef082d1f
cheficha <stefanzhekov95@gmail.com>
2017-11-26 12:22:56 +0200

    Reverse Strings

---
+++ reverseStrings/reverseStrings/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reverseStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            char[] text = str.ToCharArray();
+            char [] reverse = new char [text.Length];
+            int a = 0;
+            for (int i = text.Length-1; i >=0; i--)
+            {
+                reverse[a]=text[i];
+                a++;
+            }
+            for (int j = 0; j < reverse.Length; j++)
+            {
+                Console.Write(reverse[j]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
