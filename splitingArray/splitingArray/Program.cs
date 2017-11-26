eef08891348a5be55e4d3810c2ad65f80e7c0985
cheficha <stefanzhekov95@gmail.com>
2017-11-26 17:50:11 +0200

    splitting array

---
+++ splitingArray/splitingArray/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace splitingArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string upCase = "<upcase>";
+            string upCaseEnd = "</upcase>";
+            
+            int start = text.IndexOf(upCase);
+            int stop = text.IndexOf(upCaseEnd);
+            
+            
+
+            while (start  != -1 && stop != -1)
+            {
+                string change = text.Substring(start + 8, stop-(start+8)).ToUpper();
+                text = text.Replace(text.Substring(start, (stop+9)-start), change);
+                start = text.IndexOf(upCase);
+                stop = text.IndexOf(upCaseEnd);
+            }
+            Console.WriteLine(text);
+           
+        }
+    }
+}
