5eb5f48020159be200dfcd6c31d1423eac52a758
cheficha <stefanzhekov95@gmail.com>
2017-11-26 13:21:37 +0200

    SubStringInText

---
+++ Sub-String-In-Text/Sub-String-In-Text/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Sub_String_In_Text
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex regex = new Regex(Console.ReadLine());
+            string text = Console.ReadLine();
+            int counter = 0;
+            foreach (Match match in regex.Matches(text))
+            {
+                counter++;
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
