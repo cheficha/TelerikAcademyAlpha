52b732905d54e3ea1ac65b6c0773b2efe4ddc6a7
cheficha <stefanzhekov95@gmail.com>
2017-11-26 17:52:23 +0200

    ExtractSentences

---
+++ ExtractSenteces/ExtractSenteces/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtractSenteces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            string text = Console.ReadLine();
+            string [] sentences = text.Split('.');
+            StringBuilder str = new StringBuilder();
+            for (int i = 0; i < sentences.Length; i++)
+            {
+               string [] words = sentences[i].Split(' ',',','-','+','!','/','[',']','(',')','!',';',':','*','%','&','@','^','_','=','<','>','?','~','`','{','}');
+                for (int j = 0; j < words.Length; j++)
+                {
+                    if (word.Equals(words[j]))
+                    {
+                        str.Append(sentences[i].Trim()+". ");
+                        
+                        
+                    }
+                }
+
+            }
+            Console.Write(str.ToString().TrimEnd());
+        }
+    }
+}
