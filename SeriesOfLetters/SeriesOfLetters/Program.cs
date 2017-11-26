a26aed0caafd8e8945c19916f9b4c20dabc4005c
cheficha <stefanzhekov95@gmail.com>
2017-11-26 19:26:32 +0200

    SeriesOfLetterNotRepeating

---
+++ SeriesOfLetters/SeriesOfLetters/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeriesOfLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            char[] singleChar = input.ToCharArray();
+            List<char> final = new List<char>();
+            for (int i = 0; i <singleChar.Length-1; i++)
+            {
+                
+                    if (singleChar[i]!=singleChar[i+1])
+                    {
+                        final.Add(singleChar[i]);
+                    }
+                
+            }
+            for (int j = 0; j <final.Count; j++)
+            {
+                Console.Write(final[j]);
+            }
+          
+                Console.Write(singleChar[singleChar.Length-1]);
+         
+            Console.WriteLine();
+        }
+    }
+}
