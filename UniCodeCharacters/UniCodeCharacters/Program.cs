e66d7cb86d15950dc24f28335edcd7ef05b2e846
cheficha <stefanzhekov95@gmail.com>
2017-11-26 18:35:53 +0200

    UniCoeChar

---
+++ UniCodeCharacters/UniCodeCharacters/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniCodeCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            char [] symbols = text.ToCharArray();
+            for (int i = 0; i < symbols.Length; i++)
+            {
+               string  symbol = " ";
+                symbol = "\\u" + ((int)symbols[i]).ToString("X").PadLeft(4, '0');               
+                Console.Write(symbol);
+            }
+            
+            
+        }
+    }
+}
