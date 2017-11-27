7844843d420afe693614c30a83f95f0bfe120795
cheficha <stefanzhekov95@gmail.com>
2017-11-27 20:27:44 +0200

    reversedNumbersMethods

---
+++ ReversedNumbersMethods/ReversedNumbersMethods/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReversedNumbersMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+            ReverseNumber(number);
+        }
+
+        private static void ReverseNumber(string number)
+        {
+            char[] reversed = number.ToCharArray();
+            
+            for (int i = reversed.Length-1; i >=0; i--)
+            {
+                Console.Write(reversed[i]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
