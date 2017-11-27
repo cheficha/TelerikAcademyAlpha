6a291050056c59f255c063904852ee2c8e30ff57
cheficha <stefanzhekov95@gmail.com>
2017-11-27 20:28:03 +0200

    theFirstLargerNeighbourMethod

---
+++ TheFirstLargerNeighbour/TheFirstLargerNeighbour/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheFirstLargerNeighbour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            int[] arr = Console.ReadLine().Split(' ').Take(size).Select(int.Parse).ToArray();
+            FirstLargerNeighbour(size,arr);
+        }
+
+        static void FirstLargerNeighbour(int size, int[] arr)
+        {
+            int counter = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i - 1 >= 0 && i + 1 <= arr.Length - 1)
+                {
+                    if (arr[i] > arr[i + 1] && arr[i] > arr[i - 1])
+                    {
+                        Console.WriteLine(i);
+                        break;
+                    }
+                }
+            }
+           
+        }
+    }
+}
