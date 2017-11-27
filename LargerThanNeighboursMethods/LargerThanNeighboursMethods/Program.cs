42ebc2952a08938e7d3098add5ada263fb456085
cheficha <stefanzhekov95@gmail.com>
2017-11-27 20:25:51 +0200

    largerThanNeighbourMethods

---
+++ LargerThanNeighboursMethods/LargerThanNeighboursMethods/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LargerThanNeighboursMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = Console.ReadLine().Split(' ').Take(n).Select(int.Parse).ToArray();
+            LargerThanNextOrBefore(n,arr);
+        }
+
+           static void LargerThanNextOrBefore(int size,int [] arr)
+        {
+            int counter = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i - 1 >= 0 && i + 1 <= arr.Length - 1)
+                {
+                    if (arr[i] > arr[i + 1] && arr[i] > arr[i - 1])
+                    {
+                        counter++;
+                    }
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
