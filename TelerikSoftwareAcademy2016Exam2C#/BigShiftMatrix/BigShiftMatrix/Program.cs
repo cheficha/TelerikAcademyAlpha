fa52159486921dd239163044921f91dac5be8505
cheficha <stefanzhekov95@gmail.com>
2017-12-02 16:46:15 +0200

    BigShiftMatrix

---
+++ TelerikSoftwareAcademy2016Exam2C#/BigShiftMatrix/BigShiftMatrix/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace BigShiftMatrix
+{
+
+    class Program
+    {
+        static int Rows;
+        static int Cols;
+        static void Main(string[] args)
+        {
+            Rows = int.Parse(Console.ReadLine());
+            Cols = int.Parse(Console.ReadLine());
+            var Moves = int.Parse(Console.ReadLine());
+            var matrix = new BigInteger[Rows, Cols];
+            fillTheMatrix(matrix);
+
+        }
+
+        private static void fillTheMatrix(BigInteger[,] fill)
+        {
+
+        }
+    }
+}
