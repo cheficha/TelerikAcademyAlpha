a69eb287f094c46db01c909a3b25f32c46dc0b1f
cheficha <stefanzhekov95@gmail.com>
2017-12-03 13:55:22 +0200

    BIgShiftMatrix Done

--- TelerikSoftwareAcademy2016Exam2C#/BigShiftMatrix/BigShiftMatrix/Program.cs
+++ TelerikSoftwareAcademy2016Exam2C#/BigShiftMatrix/BigShiftMatrix/Program.cs
@@ -19,12 +19,73 @@ namespace BigShiftMatrix
             var Moves = int.Parse(Console.ReadLine());
             var matrix = new BigInteger[Rows, Cols];
             fillTheMatrix(matrix);
+            var COEF = Rows >= Cols ?
+                      Rows : Cols;
+            var CODES = Console.ReadLine()
+               .Trim()
+               .Split()
+               .Select(int.Parse)
+               .ToArray();
+            var CurrentRow = Rows - 1;
+            var CurrentCol = 0;
+            BigInteger sum = 0;
+            foreach (var code in CODES)
+            {
+                var targetRow = code / COEF;
+                var targetCol = code % COEF;
 
+                
+                for (int move = Math.Min(CurrentCol, targetCol);
+                         move <= Math.Max(CurrentCol, targetCol);
+                         move++)
+                {
+                    sum += matrix[CurrentRow, move];
+                    matrix[CurrentRow, move] = 0;
+                }
+
+               CurrentCol = targetCol;
+
+               
+                for (int move = Math.Min(CurrentRow, targetRow);
+                         move <= Math.Max(CurrentRow, targetRow);
+                         move++)
+                {
+                    sum += matrix[move, CurrentCol];
+                    matrix[move, CurrentCol] = 0;
+                }
+
+                CurrentRow = targetRow;
+            }
+
+            Console.WriteLine(sum);
         }
+    
 
         private static void fillTheMatrix(BigInteger[,] fill)
         {
+            fill[0, 0] =(long)Math.Pow( 2 , (fill.GetLength(0) - 1));
+            
 
+            //first row
+            for (int j = 1; j < fill.GetLength(1); j++)
+                {
+                int i = 0;
+                fill[i, j] = fill[i, j - 1] * 2;
+              
+                }
+           
+            //the other rows
+            for (int i = 1; i < fill.GetLength(0); i++)
+            {
+                fill[i, 0] = fill[i - 1, 0] / 2;
+                for (int j = 1; j < fill.GetLength(1); j++)
+                {
+                    fill[i, j] = fill[i, j - 1] * 2;
+                  
+                }
+              
+            }
+            
         }
     }
 }
