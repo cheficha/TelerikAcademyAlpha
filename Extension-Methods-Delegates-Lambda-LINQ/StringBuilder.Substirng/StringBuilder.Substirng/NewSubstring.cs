cfa1a5301c1f1395731b0dd642f9075c86d97f93
cheficha <stefanzhekov95@gmail.com>
2017-12-18 20:51:11 +0200

    SubBuilder

---
+++ Extension-Methods-Delegates-Lambda-LINQ/StringBuilder.Substirng/StringBuilder.Substirng/NewSubstring.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NEWsubstring
+{
+    public static class NewSubstring
+    {
+        public static StringBuilder SubBuilder(this StringBuilder str,int index,int length)
+        {
+            var newSB = new StringBuilder();
+           newSB.Append(str.ToString().Substring(index, length));
+            return newSB;
+
+        }
+    }
+}
---
+++ Extension-Methods-Delegates-Lambda-LINQ/StringBuilder.Substirng/StringBuilder.Substirng/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NEWsubstring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sb = new StringBuilder();
+            sb.Append("Az");
+            sb.Append("sum");
+            sb.Append("mnogo");
+            sb.Append("gotin");
+            sb.Append("ve");
+            sb.Append("brat");
+            Console.WriteLine( sb.SubBuilder(0,8));
+
+        }
+    }
+}
