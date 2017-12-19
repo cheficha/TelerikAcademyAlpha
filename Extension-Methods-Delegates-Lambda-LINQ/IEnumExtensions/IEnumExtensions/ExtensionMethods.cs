a82057261ec6b5ab90d4b22c6f45a01a9d9d0c7d
cheficha <stefanzhekov95@gmail.com>
2017-12-19 15:41:16 +0200

    random

---
+++ Extension-Methods-Delegates-Lambda-LINQ/IEnumExtensions/IEnumExtensions/ExtensionMethods.cs
@@ -0,0 +1,26 @@
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace IEnumExtensions
+//{
+//    public static class ExtensionMethods
+//    {
+//        public static T Sum<T>(this IEnumerable<T> collection) 
+//        {
+//            if (collection.GetType())
+//            {
+
+
+//                foreach (var item in collection)
+//                {
+//                    var sum+= item;
+//                }
+//            }
+
+//        }
+//    }
+//}
---
+++ Extension-Methods-Delegates-Lambda-LINQ/IEnumExtensions/IEnumExtensions/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEnumExtensions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "";
+            Console.WriteLine(a.GetType());
+           
+        }
+    }
+}
