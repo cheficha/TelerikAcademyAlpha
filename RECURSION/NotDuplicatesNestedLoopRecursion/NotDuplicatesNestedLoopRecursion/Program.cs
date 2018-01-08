d9a292cacee994bcc4fdf3b43bc4a1114db17906
cheficha <stefanzhekov95@gmail.com>
2018-01-08 17:34:44 +0200

    NotDuplicatesRecursion

---
+++ RECURSION/NotDuplicatesNestedLoopRecursion/NotDuplicatesNestedLoopRecursion/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotDuplicatesNestedLoopRecursion
+{
+    class Program
+    {
+        static int LoopsN;
+        static int CountK;
+        static int[] loops;
+       
+            static void Main(string[] args)
+            {
+                Console.WriteLine("N: ");
+                LoopsN = int.Parse(Console.ReadLine());
+                Console.WriteLine("K:");
+                CountK = int.Parse(Console.ReadLine());
+                loops = new int[LoopsN];
+                NotDuplicates(0);
+
+            }
+            public static void NotDuplicates(int currentLoop)
+            {
+                if (currentLoop == LoopsN)
+                {
+                    Print();
+                    return;
+                }
+                for (int i = 1; i <= CountK; i++)
+                {
+                    loops[currentLoop] = i;
+                    NotDuplicates(currentLoop + 1);
+                }
+            }
+            public static void Print()
+            {
+                for (int i = 0; i < loops.Length; i++)
+                {
+                    if (i + 1 <= loops.Length - 1)
+                    {
+                        if (loops[i] != loops[i + 1])
+                        {
+                            Console.Write(loops[i] + " " + loops[i + 1]);
+                            Console.WriteLine();
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+
