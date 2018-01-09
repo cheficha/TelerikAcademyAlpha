0d9fbdd1282936f0bc3b5c766ed280f0b0f77d8b
cheficha <stefanzhekov95@gmail.com>
2018-01-09 11:28:21 +0200

    SortingAlgorithm

---
+++ SortingAndSearchingLection/SortingAndSearchingLection/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAndSearchingLection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rand = new Random();
+            var arr = Enumerable.Range(1, 1000).Select(x =>x=rand.Next(0,1001)).ToArray();
+            Selection<int>.Sort(arr);
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
---
+++ SortingAndSearchingLection/SortingAndSearchingLection/Selection.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAndSearchingLection
+{
+    public class Selection<T> where T : IComparable<T>
+    {
+        public static void Sort(T[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                var min = i;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j].CompareTo(arr[min]) < 0)
+                    {
+                        min = j;
+                    }
+                }
+                Swap(min,i,arr);
+
+            }
+        }
+        private static void Swap(int i, int j, T[] arr)
+        {
+            var temp = arr[j];
+            arr[j] = arr[i];
+            arr[i] = temp;
+        }
+    }
+}
