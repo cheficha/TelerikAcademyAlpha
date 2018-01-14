ae7f773a7e7d1cdc54b63eaf887abad6078dea9c
cheficha <stefanzhekov95@gmail.com>
2018-01-14 13:07:01 +0200

    LDS(list,Stack)

---
+++ Linear1/Linear1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linear1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<int>();
+            var sum = 0;
+            bool check = true;
+            while (check != false)
+            {
+                string element = Console.ReadLine();
+                
+              check= int.TryParse(element,out Int32 num);
+               if (check==true)
+                {
+                    list.Add(int.Parse(element));
+                }
+                
+                
+            }
+            for (int i = 0; i < list.Count; i++)
+            {
+                sum += list[i];
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ Linear2(stack)/Linear2(stack)/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linear2_stack_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int [] ints = input.Split(' ').Select(int.Parse).ToArray();
+            var stack = new Stack<int>();
+            for (int i = ints.Length - 1; i >= 0; i--)
+            {
+                stack.Push(ints[i]);
+                Console.WriteLine(stack.Peek());
+            }
+        }
+    }
+}
