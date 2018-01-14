9ca10be7564f453147f95c43ca662f3588628d81
cheficha <stefanzhekov95@gmail.com>
2018-01-14 13:05:44 +0200

    InClassDictionariesTraining

---
+++ CityTempDict/CityTempDict/City.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace CityTempDict
+{
+    public class City
+    {
+        private string name;
+        private int temp;
+        private BigInteger population;
+        private string country;
+        public City(string name, string country, BigInteger population, int temp)
+        {
+            this.Name = name;
+            this.Country = country;
+            this.Population = population;
+            this.Temp = temp;
+
+        }
+        public string Name { get { return this.name; } private set { this.name = value; } }
+        public string Country { get { return this.country; } private set { this.country = value; } }
+        public BigInteger Population { get { return this.population; } set { this.population = value; } }
+        public int Temp { get { return this.temp; } private set { this.temp = value; } }
+
+        public  void UpgradePopulation(City city,BigInteger upgrade)
+        {
+            city.Population += upgrade;
+            
+        }
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"Name: {this.Name}");
+            sb.AppendLine($"Country: {this.Country}");
+            sb.AppendLine($"Population: {this.Population}");
+            sb.AppendLine($"Temperature: {this.Temp}");
+            return sb.ToString().Trim();
+
+        }
+        public override bool Equals(object obj)
+        {
+            var newCity = obj as City;
+            if (this.Equals(newCity))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public override int GetHashCode()
+        {
+            return this.Name.GetHashCode();
+        }
+
+
+    }
+}
---
+++ CityTempDict/CityTempDict/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityTempDict
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var varna = new City("Varna", "Bulgaria", 300000, 25);
+            var sofia = new City("Sofia", "Bulgaria", 2000000, 21);
+            var plovdiv = new City("Plovdiv", "Bulgaria",200000, 20);
+            var london = new City("London", "UK", 4000000, 12);
+
+
+            var dict = new Dictionary<string,City>();
+
+            dict.Add("Varna",varna);
+            dict.Add("Sofia", sofia);
+            dict.Add("Plovdiv", plovdiv);
+            dict.Add("London", london);
+            dict["Varna"].UpgradePopulation(dict["Varna"], 9000);
+            foreach (var item in dict)
+            {
+                Console.WriteLine(item.Key);
+                Console.WriteLine(item.Value.ToString());
+            }
+        }
+    
+
+    }
+    
+}
