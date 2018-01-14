e0d7f0c63e15fc012d04b0a0c835cd56f44494ff
cheficha <stefanzhekov95@gmail.com>
2018-01-14 13:05:17 +0200

    CokiSkoki

---
+++ CokiSkoki/CokiSkoki/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CokiSkoki
+{
+    class Program
+    {
+        static int pos = 0;
+        static int jump;
+        static int[] seq;
+        static void Main(string[] args)
+        {
+            var num = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            var buildings = input.Split(' ').Select(int.Parse).ToArray();
+            seq = new int[num];
+            for (int i = 0; i < num; i++)
+            {
+                var A = i;
+                pos++;
+                for (int j = pos; j < buildings.Length; j++)
+                {
+                    if (i+ 1 < buildings.Length)
+                    {
+
+                        if (buildings[A] < buildings[j])
+                        {
+                            jump++;
+                            A = j;
+                        }
+                        
+                    }
+                }
+                A = 0;
+                seq[i] = jump;
+                
+               
+                jump = 0;
+            }
+            Console.WriteLine(seq.Max());
+            foreach (var item in seq)
+            {
+            
+                Console.Write(item + " ");
+
+            }
+            Console.WriteLine();
+           
+        
+        }
+
+    }
+
+
+    
+}
