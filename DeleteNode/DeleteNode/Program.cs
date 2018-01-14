484a123d3bbd96211f8232a002b6465c18bc1fde
cheficha <stefanzhekov95@gmail.com>
2018-01-14 13:06:09 +0200

    DeletingNodeLinkedList

---
+++ DeleteNode/DeleteNode/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeleteNode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var linked = new LinkedList<int>();
+            linked.AddFirst(1);
+            linked.AddAfter(linked.First, 2);
+            linked.AddBefore(linked.Last, 3);
+            linked.AddLast(4);
+            var value=linked.Find(3);
+            linked.Remove(value);
+            foreach (var item in linked)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
